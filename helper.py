df104030c402824940ccb0f4650ada773ca014ad
Dmitriy Lagutin <38376206+DmitryLagutin@users.noreply.github.com>
2020-12-19 15:28:41 +0300

    Промежуточный итог

--- helper.py
+++ helper.py
@@ -4,7 +4,6 @@ import csv
 import ipaddress
 from prettytable import PrettyTable
 
-
 main_list = []
 connection = pymysql.connect(host='dbi20.flexline.ru',
                              user='sormuser',
@@ -28,5 +27,51 @@ def main_function(first_date, last_date):
         for row in cursor:
             list_result.append(dict(count_ip=row['count(day.ip)'], sum_in=row['sum(day.cin)/1024/1024/1024'],
                                     sum_out=row['sum(day.cout)/1024/1024/1024'], id=row['id']))
+
+        return list_result
+
+
+def for_ip_func(ip, first_date, last_date):
+    with connection.cursor() as cursor:
+        sql = f'''
+                select 
+                v.login, v.current_shape, sum(d.cin)/1024/1024/1024, sum(d.cout)/1024/1024/1024
+                from day d
+                inner join vgroups v on (d.vg_id=v.vg_id)
+                inner join staff st on (v.vg_id=st.vg_id)
+                where
+                st.segment=inet_aton("{ip}") and
+                d.timefrom >= "{first_date}" and d.timefrom < "{last_date}";
+                '''
+        list_result = []
+        cursor.execute(sql)
+        for row in cursor:
+            print(dict(row))
+            list_result.append(dict(login=row['login'], current_shape=row['current_shape'],
+                                    sum_in=row['sum(d.cin)/1024/1024/1024'],
+                                    sum_out=row['sum(d.cout)/1024/1024/1024']))
+
+        return list_result
+
+
+def for_login_func(login, first_date, last_date):
+    with connection.cursor() as cursor:
+        sql = f'''
+            select 
+            v.login, v.current_shape, sum(d.cin)/1024/1024/1024, sum(d.cout)/1024/1024/1024
+            from day d
+            inner join vgroups v on (d.vg_id=v.vg_id)
+            inner join staff st on (v.vg_id=st.vg_id)
+            where
+            v.login="{login}" and 
+            d.timefrom >= "{first_date}" and d.timefrom < "{last_date}";
+            '''
+        list_result = []
+        cursor.execute(sql)
+        for row in cursor:
+            print(dict(row))
+            list_result.append(dict(login=row['login'], current_shape=row['current_shape'],
+                                    sum_in=row['sum(d.cin)/1024/1024/1024'],
+                                    sum_out=row['sum(d.cout)/1024/1024/1024']))
 
         return list_result
--- main.py
+++ main.py
@@ -1,24 +1,58 @@
 from flask import Flask, request, session, redirect, url_for, render_template
-from helper import main_function
+import pymysql.cursors
+from helper import main_function, for_ip_func, for_login_func
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
-def hello():
+def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
         first_date = request.form['first_date']
         last_date = request.form['last_date']
+        print(first_date, last_date)
         if first_date is '' or last_date is '':
             return render_template('not_date.html')
-        x = first_date.split('T')
-        y = last_date.split('T')
-        first_date = x[0] + ' ' + x[1] + ':00'
-        last_date = y[0] + ' ' + x[1] + ':00'
+        first_date = first_date + ' ' + '00:00:00'
+        last_date = last_date + ' ' + '00:00:00'
         agents = main_function(first_date, last_date)
         return render_template('good_calculate.html', agents=agents)
+
+
+@app.route("/for_ip", methods=['GET', 'POST'])
+def for_ip():
+    if request.method == 'GET':
+        return render_template('for_ip.html')
+    elif request.method == 'POST':
+        ip = request.form['ip']
+        first_date = request.form['first_date']
+        last_date = request.form['last_date']
+        print(first_date, last_date, ip)
+        if first_date is '' or last_date is '' or ip is '':
+            return render_template('not_date.html')
+        first_date = first_date + ' ' + '00:00:00'
+        last_date = last_date + ' ' + '00:00:00'
+        rows = for_ip_func(ip, first_date, last_date)
+        return render_template('good_calc_ip.html', rows=rows, ip=ip)
+
+
+@app.route("/for_login", methods=['GET', 'POST'])
+def for_login():
+    if request.method == 'GET':
+        return render_template('for_login.html')
+    elif request.method == 'POST':
+        login = request.form['login']
+        first_date = request.form['first_date']
+        last_date = request.form['last_date']
+        print(first_date, last_date, login)
+        if first_date is '' or last_date is '' or login is '':
+            return render_template('not_date.html')
+        first_date = first_date + ' ' + '00:00:00'
+        last_date = last_date + ' ' + '00:00:00'
+        rows = for_login_func(login, first_date, last_date)
+        return render_template('good_calc_login.html', rows=rows, login=login)
 
 
 if __name__ == "__main__":
